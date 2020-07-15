adab6a3cc6d8ded5f6b9ef17bc506e93a761bb60
Lourenço <loulage@gmail.com>
2020-07-15 19:22:42 -0300

    ccfix2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,26 +4,26 @@ class Login extends Component {
   render() {
     return (
       <div>
-          <p>E-mail:</p>
-          <input
-            type="email"
-            data-testid="input-gravatar-email"
-            name="email"
-            placeholder="email"
-            // onChange={}
+        <p>E-mail:</p>
+        <input
+          type="email"
+          data-testid="input-gravatar-email"
+          name="email"
+          placeholder="email"
+          // onChange={}
           />
-          <p>Nome:</p>
-          <input
-            type="text"
-            data-testid="input-player-name"
-            name="name"
-            placeholder="nome"
-            // onChange={}
-          />
+        <p>Nome:</p>
+        <input
+          type="text"
+          data-testid="input-player-name"
+          name="name"
+          placeholder="nome"
+          // onChange={}
+        />
         <button data-testid="btn-play">Jogar!</button>
       </div>
-    )
+    );
   }
 }
 
-export default Login
+export default Login;
