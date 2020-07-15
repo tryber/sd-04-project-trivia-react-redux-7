0e9ee8e02666511cc608042e7470ca8a0b634493
Lourenço <loulage@gmail.com>
2020-07-15 19:19:25 -0300

    ccfix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Login extends Component {
   render() {
     return (
       <div>
-        <label>
-          E-mail:
+          <p>E-mail:</p>
           <input
-            type='email'
-            data-testid='input-gravatar-email'
-            name='email'
-            placeholder='email'
+            type="email"
+            data-testid="input-gravatar-email"
+            name="email"
+            placeholder="email"
             // onChange={}
           />
-        </label>
-        <label>
-          Nome:
+          <p>Nome:</p>
           <input
-            type='text'
-            data-testid='input-player-name'
-            name='name'
-            placeholder='nome'
+            type="text"
+            data-testid="input-player-name"
+            name="name"
+            placeholder="nome"
             // onChange={}
           />
-        </label>
-        <button data-testid='btn-play' >Jogar!</button>
+        <button data-testid="btn-play">Jogar!</button>
       </div>
     )
   }
