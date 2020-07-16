f90928a72ffd1468d87ce3c9eac17c645bcad9f5
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-07-16 20:41:11 -0300

    voltando provider e router como era antes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
 import Feedback from './pages/Feedback';
 import Game from './pages/Game';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Rank from './pages/Rank';
-import store from './store';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/game" component={Game} />
-          <Route exact path="/feedback" component={Feedback} />
-          <Route exact path="/settings" component={Settings} />
-          <Route exact path="/rank" component={Rank} />
-          <Route exact path="/" component={Login} />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
+    <Switch>
+      <Route exact path="/game" component={Game} />
+      <Route exact path="/feedback" component={Feedback} />
+      <Route exact path="/settings" component={Settings} />
+      <Route exact path="/rank" component={Rank} />
+      <Route exact path="/" component={Login} />
+    </Switch>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-// import store from './store';
+import store from './store';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
